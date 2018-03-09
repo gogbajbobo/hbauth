c07dbbb8ecabe5b5ca8c93b5796752b8c66b304c
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-09 17:27:30 +0300

    simple check of req.headers.authorization

---
+++ app.js
@@ -0,0 +1,31 @@
+const
+    debug = require('debug')('hbauth:app'),
+    express = require('express'),
+    app = express();
+
+const validator = (req, res, next) => {
+
+    debug('authorization token:', req.headers.authorization);
+
+    if (req.headers.authorization === 'authKey_12345') {
+
+        debug('ok — 200');
+        res.status(200).send();
+
+    } else {
+
+        debug('not ok — 403');
+        res.status(403).send({error: 'forbidden'});
+
+    }
+
+    return next();
+
+};
+
+app.use('/', validator);
+
+const port = 8887;
+app.listen(port, () => {
+    debug('HomeBudget Auth server started on: ' + port);
+});
