1fa7edf11e713ff2f52b78a7b9c373292b2ecb53
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-10 19:53:06 +0300

    make login works

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,8 @@ const
     debug = require('debug')('hbauth:login'),
     router = require('express').Router(),
     bcrypt = require('bcrypt'),
-    knex = require('../db/knex');
+    users = require('../db/knex')('users'),
+    _ = require('lodash');
 
 module.exports = () => {
 
@@ -20,9 +21,33 @@ function login() {
             const login = req.body.login || req.query.login;
             const password = req.body.password || req.query.password;
 
-            debug(login, password);
-            res.status(200).json({login, password});
+            debug('login user:', login, 'and password:', password);
+
+            if (!login || !password) {
+                return res.status(400).send();
+            }
+
+            users.select('hash').where('login', login)
+                .then(result => {
+
+                    const hash = _.first(_.flatMap(result, obj => _.values(obj)));
+
+                    if (!hash) {
+                        return res.status(401).json({error: true, message: 'login unauthorized'});
+                    }
+
+                    bcrypt.compare(password, hash, (err, success) => {
+
+                        if (!err && success) {
+                            res.status(200).json({login, password, hash, message: 'login success'});
+                        } else {
+                            res.status(401).json({error: true, message: 'login unauthorized'});
+                        }
+
+                    });
+
+                });
 
         })
-
+    
 }
