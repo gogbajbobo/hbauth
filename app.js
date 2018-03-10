931aacb2a61e399cd78df4a9e8e5533d48704181
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-10 17:47:03 +0300

    npm body-parser / bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const
     debug = require('debug')('hbauth:app'),
     express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser');
 
 const validator = require('./routes/check');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(validator);
 
