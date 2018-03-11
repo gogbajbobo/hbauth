205d80b79c841a189ec5f10980ceaaca6cc08da7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-11 17:47:53 +0300

    rm oauth2-server

--- app.js
+++ app.js
@@ -4,19 +4,10 @@ const
     app = express(),
     router = express.Router(),
     knex = require('./db/config/knex'),
-    bodyParser = require('body-parser'),
-    oauthserver = require('oauth2-server');
+    bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// app.oauth = oauthserver({
-//     model: {}, // See below for specification
-//     grants: ['password'],
-//     debug: true
-// });
-
-// app.use(app.oauth.errorHandler());
 
 const register = require('./routes/register')(router, app, knex);
 const login = require('./routes/login')(router, app, knex);
