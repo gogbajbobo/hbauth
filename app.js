894e9352320ddc1f0a18cbc728de8ba0d15fe556
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-11 15:10:23 +0300

    npm oauth2-server

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const
     debug = require('debug')('hbauth:app'),
     express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const register = require('./routes/register')();
 const login = require('./routes/login')();
+// app.oauth = oauthserver({
+//     model: {}, // See below for specification
+//     grants: ['password'],
+//     debug: true
+// });
+
+// app.use(app.oauth.errorHandler());
 app.use(register);
 app.use(login);
 
