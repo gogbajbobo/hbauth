3ef927a70fedbcc08e4c713dd57e1064a21df0f6
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-12 12:06:32 +0300

    test passport.authenticate

--- app.js
+++ app.js
@@ -11,17 +11,28 @@ const
     passportHelper = require('./auth/passport/passportHelper')(usersDbHelper);
 
 passport.use(new Strategy(passportHelper.passportLocalVerify));
+passport.serializeUser(passportHelper.serializeUser);
+passport.deserializeUser(passportHelper.deserializeUser);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 const
-    register = require('./routes/register')(router, app, knex),
-    login = require('./routes/login')(router, app, knex),
+    // register = require('./routes/register')(router, app, knex),
+    // login = require('./routes/login')(router, app, knex),
     validator = require('./routes/check');
 
-app.use(register);
-app.use(login);
+// app.use(register);
+// app.use(login);
+app.use('/login',
+    passport.authenticate('local', { failureRedirect: '/register' }),
+    (req, res) => {
+        res.redirect('/');
+    });
+
 app.use(validator);
 
 const port = 8887;
